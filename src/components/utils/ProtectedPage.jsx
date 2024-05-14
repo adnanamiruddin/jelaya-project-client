23b2225f0aa75e6acf7d2f6340a5fbaa7c71684b
adnanamiruddin <adnan.amiruddin34@gmail.com>
2024-05-14 13:46:46 +0800

    fix: Fix bug on ProtectedPage component"

--- src/components/utils/ProtectedPage.jsx
+++ src/components/utils/ProtectedPage.jsx
@@ -1,20 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "@/redux/features/userSlice";
 import { useRouter } from "next/router";
 import { toast } from "react-toastify";
+import AnimationLoading from "../layouts/AnimationLoading";
 
 const ProtectedPage = ({ children }) => {
   const router = useRouter();
-
   const { user } = useSelector(selectUser);
+  const [isChecking, setIsChecking] = useState(true);
 
   useEffect(() => {
-    if (!user) {
-      router.push("/login");
-      toast.error("Anda harus masuk terlebih dahulu");
-    }
+    const checkUser = setTimeout(() => {
+      if (!user) {
+        router.push("/login");
+        toast.error("Anda harus masuk terlebih dahulu");
+      }
+      setIsChecking(false);
+    }, 3000);
+
+    return () => clearTimeout(checkUser);
   }, [user, router]);
+
+  if (isChecking) {
+    return <AnimationLoading />;
+  }
 
   return user ? children : null;
 };
