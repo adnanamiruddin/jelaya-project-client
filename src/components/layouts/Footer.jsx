1794cacc3675a621b3e5294ef827d3cfbf76fb67
adnanamiruddin <adnan.amiruddin34@gmail.com>
2024-05-15 05:31:12 +0800

    fix: Resize footer's padding

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div className="flex justify-between items-center bg-gray-900 py-2 px-7">
+    <div className="flex justify-between items-center bg-gray-900 py-4 px-7">
       <div className="flex gap-5 items-center md:gap-7">
         <Image
           src="/logo-jelaya.jpeg"
           alt="Logo Pantai Marina"
           width={100}
           height={100}
-          className="w-14 h-14"
+          className="w-14 h-14 rounded-md"
         />
 
         <p className="text-gray-500 text-sm">
