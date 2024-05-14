8dda589b3835fcc9cd73f16144241ac2853526ab
adnanamiruddin <adnan.amiruddin34@gmail.com>
2024-05-15 05:11:18 +0800

    fix: Fix wrong code

--- src/pages/dashboard/my-blogs/[id].jsx
+++ src/pages/dashboard/my-blogs/[id].jsx
@@ -228,242 +228,237 @@ export default function MyBlogDetail() {
   return (
     <ProtectedPage>
       {isDataLoaded ? (
-        <ProtectedPage>
-          <div className="md:px-12 md:mt-6">
-            <div className="mb-6 flex flex-col justify-center items-center">
-              <div className="flex ms-auto items-center gap-4">
-                <h5 className="font-bold text-xl">Edit Mode</h5>
-                <label class="switch">
-                  <input
-                    class="cb"
-                    type="checkbox"
-                    onChange={(e) => {
-                      setIsEditMode(e.target.checked);
-                    }}
-                  />
-                  <span class="toggle">
-                    <span class="left">off</span>
-                    <span class="right">on</span>
-                  </span>
-                </label>
-              </div>
+        <div className="md:px-12 md:mt-6">
+          <div className="mb-6 flex flex-col justify-center items-center">
+            <div className="flex ms-auto items-center gap-4">
+              <h5 className="font-bold text-xl">Edit Mode</h5>
+              <label class="switch">
+                <input
+                  class="cb"
+                  type="checkbox"
+                  onChange={(e) => {
+                    setIsEditMode(e.target.checked);
+                  }}
+                />
+                <span class="toggle">
+                  <span class="left">off</span>
+                  <span class="right">on</span>
+                </span>
+              </label>
             </div>
+          </div>
 
-            <form onSubmit={editBlogForm.handleSubmit}>
-              <input
-                type="text"
-                name="title"
-                disabled={!isEditMode}
-                placeholder="Budaya Indonesia..."
-                value={editBlogForm.values.title}
-                onChange={editBlogForm.handleChange}
-                className={`w-full bg-white text-black text-2xl text-center font-bold border-2 rounded-md py-2.5 md:text-5xl ${
-                  !isEditMode
-                    ? "cursor-not-allowed border-white"
-                    : "border-teal-500"
-                }`}
-              />
+          <form onSubmit={editBlogForm.handleSubmit}>
+            <input
+              type="text"
+              name="title"
+              disabled={!isEditMode}
+              placeholder="Budaya Indonesia..."
+              value={editBlogForm.values.title}
+              onChange={editBlogForm.handleChange}
+              className={`w-full bg-white text-black text-2xl text-center font-bold border-2 rounded-md py-2.5 md:text-5xl ${
+                !isEditMode
+                  ? "cursor-not-allowed border-white"
+                  : "border-teal-500"
+              }`}
+            />
 
-              {isEditMode ? (
-                <>
-                  <div className="mt-2 w-full">
+            {isEditMode ? (
+              <>
+                <div className="mt-2 w-full">
+                  <label className="label">
+                    <span className="label-text text-black text-lg">
+                      Kategori
+                    </span>
+                  </label>
+                  <select
+                    onChange={(e) => {
+                      editBlogForm.setFieldValue("category", e.target.value);
+                    }}
+                    name="category"
+                    className="select select-accent w-full bg-gray-50"
+                  >
+                    <option selected>{editBlogForm.values.category}</option>
+                    <option disabled>---</option>
+                    <option>Adat Istiadat</option>
+                    <option>Bahasa</option>
+                    <option>Makanan Tradisional</option>
+                  </select>
+                </div>
+
+                <div className="mt-2 mb-6 flex gap-2">
+                  <div className="w-1/2">
                     <label className="label">
                       <span className="label-text text-black text-lg">
-                        Kategori
+                        Provinsi
                       </span>
                     </label>
                     <select
-                      onChange={(e) => {
-                        editBlogForm.setFieldValue("category", e.target.value);
-                      }}
-                      name="category"
+                      onChange={handleProvinceSelectedChange}
+                      name="province"
                       className="select select-accent w-full bg-gray-50"
                     >
-                      <option selected>{editBlogForm.values.category}</option>
+                      <option
+                        selected
+                        disabled={editBlogForm.values.province !== ""}
+                      >
+                        {editBlogForm.values.province}
+                      </option>
                       <option disabled>---</option>
-                      <option>Adat Istiadat</option>
-                      <option>Bahasa</option>
-                      <option>Makanan Tradisional</option>
+                      {provincesApi.map((province) => (
+                        <option key={province.id} value={province.id}>
+                          {province.name}
+                        </option>
+                      ))}
                     </select>
                   </div>
 
-                  <div className="mt-2 mb-6 flex gap-2">
-                    <div className="w-1/2">
-                      <label className="label">
-                        <span className="label-text text-black text-lg">
-                          Provinsi
-                        </span>
-                      </label>
-                      <select
-                        onChange={handleProvinceSelectedChange}
-                        name="province"
-                        className="select select-accent w-full bg-gray-50"
-                      >
-                        <option
-                          selected
-                          disabled={editBlogForm.values.province !== ""}
-                        >
-                          {editBlogForm.values.province}
+                  <div className="w-1/2">
+                    <label className="label">
+                      <span className="label-text text-black text-lg">
+                        Kota
+                      </span>
+                    </label>
+                    <select
+                      disabled={isCitiesApiLoading}
+                      onChange={handleCitySelectedChange}
+                      name="city"
+                      className="select select-accent w-full bg-gray-50"
+                    >
+                      <option selected hidden={citiesApi.length > 0}>
+                        {editBlogForm.values.city}
+                      </option>
+                      <option disabled>---</option>
+                      {citiesApi.map((city) => (
+                        <option key={city.id} value={city.id}>
+                          {city.name}
                         </option>
-                        <option disabled>---</option>
-                        {provincesApi.map((province) => (
-                          <option key={province.id} value={province.id}>
-                            {province.name}
-                          </option>
-                        ))}
-                      </select>
-                    </div>
-
-                    <div className="w-1/2">
-                      <label className="label">
-                        <span className="label-text text-black text-lg">
-                          Kota
-                        </span>
-                      </label>
-                      <select
-                        disabled={isCitiesApiLoading}
-                        onChange={handleCitySelectedChange}
-                        name="city"
-                        className="select select-accent w-full bg-gray-50"
-                      >
-                        <option selected hidden={citiesApi.length > 0}>
-                          {editBlogForm.values.city}
-                        </option>
-                        <option disabled>---</option>
-                        {citiesApi.map((city) => (
-                          <option key={city.id} value={city.id}>
-                            {city.name}
-                          </option>
-                        ))}
-                      </select>
-                    </div>
+                      ))}
+                    </select>
                   </div>
-                </>
-              ) : null}
+                </div>
+              </>
+            ) : null}
 
-              <div className="flex justify-center">
-                <Image
-                  src={editBlogForm.values.blogImageUrl}
-                  alt={editBlogForm.values.title}
-                  width={500}
-                  height={500}
-                  className="mt-2 w-full h-full object-cover rounded-md md:mt-6 md:w-[75%] md:object-contain"
-                />
-              </div>
+            <div className="flex justify-center">
+              <Image
+                src={editBlogForm.values.blogImageUrl}
+                alt={editBlogForm.values.title}
+                width={500}
+                height={500}
+                className="mt-2 w-full h-full object-cover rounded-md md:mt-6 md:w-[75%] md:object-contain"
+              />
+            </div>
 
-              <div className="mt-8 flex flex-col justify-center items-center gap-5">
-                {existingBlogContents.map((blogContent) => {
-                  if (blogContent.type === "text") {
-                    return (
-                      <div
-                        key={blogContent.id}
-                        className="relative w-full md:w-[75%]"
-                      >
-                        <MdDeleteForever
-                          onClick={() => {
-                            handleDeleteBlogContent(
-                              blogContent.blogId,
-                              blogContent.id,
-                              blogContent.type
-                            );
-                          }}
-                          className="absolute top-2 right-2 text-2xl text-red-600"
-                        />
-                        <textarea
-                          defaultValue={blogContent.text}
-                          onChange={(e) => {
-                            const updatedBlogContents =
-                              existingBlogContents.map(
-                                (existingBlogContent) => {
-                                  if (
-                                    existingBlogContent.id === blogContent.id
-                                  ) {
-                                    return {
-                                      ...existingBlogContent,
-                                      text: e.target.value,
-                                    };
-                                  }
-                                  return existingBlogContent;
-                                }
-                              );
-                            setExistingBlogContents(updatedBlogContents);
-                          }}
-                          rows={4}
-                          className="textarea textarea-lg w-full bg-white text-black border-teal-400"
-                        ></textarea>
-                      </div>
-                    );
-                  } else if (blogContent.type === "image") {
-                    return (
-                      <div
-                        key={blogContent.id}
-                        className="relative w-full md:w-[75%] md:object-contain"
-                      >
-                        <MdDeleteForever
-                          onClick={() => {
-                            handleDeleteBlogContent(
-                              blogContent.blogId,
-                              blogContent.id,
-                              blogContent.type
-                            );
-                          }}
-                          className="absolute top-2 right-2 text-2xl text-white bg-red-600 rounded p-0.5"
-                        />
-                        <Image
-                          src={blogContent.imageUrl}
-                          alt={blogContent.type}
-                          width={500}
-                          height={500}
-                          className="w-full h-full object-cover rounded-md"
-                        />
-                      </div>
-                    );
-                  }
-                })}
-              </div>
-
-              <button
-                type="button"
-                onClick={() =>
-                  document
-                    .getElementById("choose_blog_content_type_modal")
-                    .showModal()
+            <div className="mt-8 flex flex-col justify-center items-center gap-5">
+              {existingBlogContents.map((blogContent) => {
+                if (blogContent.type === "text") {
+                  return (
+                    <div
+                      key={blogContent.id}
+                      className="relative w-full md:w-[75%]"
+                    >
+                      <MdDeleteForever
+                        onClick={() => {
+                          handleDeleteBlogContent(
+                            blogContent.blogId,
+                            blogContent.id,
+                            blogContent.type
+                          );
+                        }}
+                        className="absolute top-2 right-2 text-2xl text-red-600"
+                      />
+                      <textarea
+                        defaultValue={blogContent.text}
+                        onChange={(e) => {
+                          const updatedBlogContents = existingBlogContents.map(
+                            (existingBlogContent) => {
+                              if (existingBlogContent.id === blogContent.id) {
+                                return {
+                                  ...existingBlogContent,
+                                  text: e.target.value,
+                                };
+                              }
+                              return existingBlogContent;
+                            }
+                          );
+                          setExistingBlogContents(updatedBlogContents);
+                        }}
+                        rows={4}
+                        className="textarea textarea-lg w-full bg-white text-black border-teal-400"
+                      ></textarea>
+                    </div>
+                  );
+                } else if (blogContent.type === "image") {
+                  return (
+                    <div
+                      key={blogContent.id}
+                      className="relative w-full md:w-[75%] md:object-contain"
+                    >
+                      <MdDeleteForever
+                        onClick={() => {
+                          handleDeleteBlogContent(
+                            blogContent.blogId,
+                            blogContent.id,
+                            blogContent.type
+                          );
+                        }}
+                        className="absolute top-2 right-2 text-2xl text-white bg-red-600 rounded p-0.5"
+                      />
+                      <Image
+                        src={blogContent.imageUrl}
+                        alt={blogContent.type}
+                        width={500}
+                        height={500}
+                        className="w-full h-full object-cover rounded-md"
+                      />
+                    </div>
+                  );
                 }
-                className="mt-4 flex items-center bg-teal-500 text-white px-4 py-2 rounded-md md:mx-[10%]"
-              >
-                <IoIosAddCircleOutline className="mr-2 text-2xl" />
-                Tambah Konten Blog
-              </button>
+              })}
+            </div>
 
-              <div
-                id="blog_contents"
-                className="mt-4 flex flex-col gap-8 md:items-center md:justify-center"
-              ></div>
+            <button
+              type="button"
+              onClick={() =>
+                document
+                  .getElementById("choose_blog_content_type_modal")
+                  .showModal()
+              }
+              className="mt-4 flex items-center bg-teal-500 text-white px-4 py-2 rounded-md md:mx-[10%]"
+            >
+              <IoIosAddCircleOutline className="mr-2 text-2xl" />
+              Tambah Konten Blog
+            </button>
 
-              <button
-                type="submit"
-                className={`mt-6 flex items-center bg-teal-500 text-white px-4 py-2 rounded-md w-full justify-center md:w-max md:mx-[10%] ${
-                  isOnRequest ? "bg-teal-700" : ""
-                }`}
-              >
-                {!isOnRequest ? (
-                  <>
-                    <FaRegSave className="mr-2 text-2xl" />
-                    Simpan
-                  </>
-                ) : (
-                  <span className="loading loading-bars loading-md"></span>
-                )}
-              </button>
-            </form>
+            <div
+              id="blog_contents"
+              className="mt-4 flex flex-col gap-8 md:items-center md:justify-center"
+            ></div>
 
-            <ChooseBlogContentTypeModal
-              setAddBlogContents={setAddBlogContents}
-              uploadImage={uploadImage}
-              setUploadImage={setUploadImage}
-            />
-          </div>
-        </ProtectedPage>
+            <button
+              type="submit"
+              className={`mt-6 flex items-center bg-teal-500 text-white px-4 py-2 rounded-md w-full justify-center md:w-max md:mx-[10%] ${
+                isOnRequest ? "bg-teal-700" : ""
+              }`}
+            >
+              {!isOnRequest ? (
+                <>
+                  <FaRegSave className="mr-2 text-2xl" />
+                  Simpan
+                </>
+              ) : (
+                <span className="loading loading-bars loading-md"></span>
+              )}
+            </button>
+          </form>
+
+          <ChooseBlogContentTypeModal
+            setAddBlogContents={setAddBlogContents}
+            uploadImage={uploadImage}
+            setUploadImage={setUploadImage}
+          />
+        </div>
       ) : (
         <AnimationLoading />
       )}
